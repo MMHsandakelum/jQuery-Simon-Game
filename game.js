fb9e58e6c3bc0f7fb7e71556626f5b7bf888b526
Harsha Sandakelum <mmharshasandakelum@gmail.com>
2023-10-29 15:38:51 +0530

    final commit

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
 
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
@@ -8,16 +9,55 @@ $(".btn").click(function () {
   animatePress(userChosenColour);
   playSound(userChosenColour);
   console.log(userClickedPattern);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).on('keypress', function (event) {
+  $("h1").text("Level 0");
+  nextSequence();
 });
 
 function nextSequence() {
+  userClickedPattern = [];
+  $("h1").text("Level " + level);
+  level = level + 1;
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   animatePress(randomChosenColour);
   playSound(randomChosenColour);
   console.log(gamePattern);
+  console.log(level);
 }
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (gamePattern.length === userClickedPattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+    console.log("success");
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").attr("class", "game-over");
+    setTimeout(function () {
+      $("body").removeAttr("class", "game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+
+}
+
 
 function playSound(randomChosenColour) {
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
